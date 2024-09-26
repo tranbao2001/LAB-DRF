d33de528b42710d4b026028ae9cf7eb8979d3d55
tranbao2001 <bao.tb2k1@gmail.com>
2024-09-26 11:33:06 +0700

    tutorial07

--- tranbaoLAB1/mysite/settings.py
+++ tranbaoLAB1/mysite/settings.py
@@ -130,3 +130,18 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
